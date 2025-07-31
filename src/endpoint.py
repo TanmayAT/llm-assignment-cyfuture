56d6dd08286779a977a462dd128c58c0c7628108
TanmayAT <Vaidikpandeytt@gmail.com>
2025-08-01 04:09:53 +0530

    Update __init__.py

--- src/endpoint.py
+++ src/endpoint.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware import Middleware
 from pydantic import BaseModel, Field
 from typing import Optional
-from .model import text_generator
+from model import text_generator
 
 # Define request/response models
 class TextGenerationRequest(BaseModel):
